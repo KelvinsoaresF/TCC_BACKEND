22ef793812d1ce5a367caf0626f8de56512e97a4
kelvin Felix  Soares da Silva <kelvinf494@gmail.com>
2025-05-21 18:22:57 -0300

    .

--- backend_tcc/routes/api.php
+++ backend_tcc/routes/api.php
@@ -42,7 +42,8 @@ Route::middleware('auth:sanctum')->group(function() {
     Route::get('/my-posts', [ProfileController::class, 'myPosts'])->middleware('auth:sanctum');
     Route::put('/edit-profile', [ProfileController::class, 'editProfile'])->middleware('auth:sanctum');
 
-    Route::put('/edit-post', [AnimalPostController::class, 'update'])->middleware('auth:sanctum');
+    Route::put('/edit-post/{id}', [AnimalPostController::class, 'update'])->middleware('auth:sanctum');
+    Route::delete('/delete-post/{id}', [AnimalPostController::class, 'destroy'])->middleware('auth:sanctum');
 
     // salvar post
     Route::post('/post/{id}/save', [InteractionPostController::class, 'savePost'])->middleware('auth:sanctum');
